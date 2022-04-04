3e6554f3584331e973b69aea8a1a46ec27feb1a6
ClarenceLouise <100056421+ClarenceLouise@users.noreply.github.com>
2022-04-04 10:44:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
     <li><a href="/LT3/Top 10.html">LEARNING TASK 3</a></li>
     <li><a href="/CLE1MT/Index.html">Midterm Exam</a></li>
-    <li><a href="/LT4/index.html">Learning Task 4</a></li>
+    <li><a href="/LT4/index.html">LEARNING TASK 4</a></li>
   </ul>
   </body>
         </html>
